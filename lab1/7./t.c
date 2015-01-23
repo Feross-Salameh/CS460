8204ee0fbf276fa688add4f7c6469cd2b9cecbe7
Feross Salameh <feross.salameh@gmail.com>
2015-01-23 14:51:39 -0800

    work on 7

--- lab1/7./t.c
+++ lab1/7./t.c
@@ -1,6 +1,8 @@
-/********** t.c file ******************/
+#ifndef PART7
+#define PART7 
 
-#include "ext2.h"  /* download this from LAB1 */
+/********** t.c file ******************/
+//#include "ext2.h"  /* download this from LAB1 */
 typedef struct ext2_group_desc  GD;
 typedef struct ext2_inode       INODE;
 typedef struct ext2_dir_entry_2 DIR;
@@ -9,7 +11,49 @@ typedef unsigned char  u8;
 typedef unsigned short u16;
 typedef unsigned long  u32;
 
+
+struct ext2_inode
+{
+	u16	i_mode;		/* File mode */
+	u16	i_uid;		/* Owner Uid */
+	u32	i_size;		/* Size in bytes */
+	u32	i_atime;	/* Access time */
+	u32	i_ctime;	/* Creation time */
+	u32	i_mtime;	/* Modification time */
+	u32	i_dtime;	/* Deletion Time */
+	u16	i_gid;		/* Group Id */
+	u16	i_links_count;	/* Links count */
+	u32	i_blocks;	/* Blocks count */
+	u32	i_flags;	/* File flags */
+        u32     dummy;
+	u32	i_block[15];    /* Pointers to blocks */
+        u32     pad[7];         /* inode size MUST be 128 bytes */
+};
+
+struct ext2_group_desc
+{
+	u32	bg_block_bitmap;		/* Blocks bitmap block */
+	u32	bg_inode_bitmap;		/* Inodes bitmap block */
+	u32	bg_inode_table;		/* Inodes table block */
+	u16	bg_free_blocks_count;	/* Free blocks count */
+	u16	bg_free_inodes_count;	/* Free inodes count */
+	u16	bg_used_dirs_count;	/* Directories count */
+	u16	bg_pad;
+	u32	bg_reserved[3];
+};
+
+struct ext2_dir_entry_2 {
+	u32	inode;			/* Inode number */
+	u16	rec_len;		/* Directory entry length */
+	u8	name_len;		/* Name length */
+	u8	file_type;
+	char	name[255];      	/* File name */
+};
+
 #define BLK 1024
+
+#endif
+
 
 int prints(char *s)
 {
@@ -17,26 +61,22 @@ int prints(char *s)
 	while(*t != '\0')
 	{
 		putc(*t);
+		if(*t == '\n')
+			putc('\r');
 		t++;
 	}
-	putc('\n');
-	putc('\r');
-}
-
-int blk2cyl(u16 blk)
-{
-	return  blk / 36;
-}
-
-int blk2hd(u16 blk)
-{
-	return 
 }
 
 u16 getblk(u16 blk, char buf[ ])
 {
-   // convert blk to CHS; then call readfd(c,h,s, buf);
+	u16 block = blk;
+	u16 cylinder = 0, head = 0, sector = 0;
+	cylinder = block % 5;
+	//uh = (b % 36) / 2;
+	//c = (blk / 36);
 }
+
+
 
 
 GD    *gp;
@@ -56,7 +96,6 @@ main()
   iblk = (u16)gp->bg_inode_table; // typecast u32 to u16
 
   prints("inode_block="); putc(iblk+'0'); getc();
-
   /******** write C code to do these: ********************
   (1).read first inode block into buf1[ ]
 
@@ -68,10 +107,5 @@ main()
   
   (4).prints("\n\rAll done\n\r");
   ******************************************************/
-  prints("Starting while");
-  while(1)
-  {
-	  
-  }
   
 }  
