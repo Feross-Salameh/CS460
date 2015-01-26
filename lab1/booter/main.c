bddf4dbf581fa722e03ac869519bdf7d4e1ba9aa
Feross Salameh <feross.salameh@gmail.com>
2015-01-26 15:53:04 -0800

    work on booter

--- lab1/booter/main.c
+++ lab1/booter/main.c
@@ -13,18 +13,6 @@ int prints(char *s)
 	}
 }
 
-int printsn(char *s, int len)
-{
-	int i;
-   	char *t = s;
-   	for(i = 0; i < len; i++)
-   	{
-		putc(t[i]);
-		if(t[i] == '\n')
-			putc('\r');
-	}
-}
-
 u16 getblk(u16 blk, char buf[ ])
 {
 	
@@ -33,32 +21,44 @@ u16 getblk(u16 blk, char buf[ ])
 	head = (blk % 19) / 9;
 	cylinder = (blk / 18);
 	readfd(cylinder, head, sector, buf);
-	//readfd(blk/18, ((blk)%18)/9, (((blk)%18)%9)*2, buf); 
 }
 
-u16 printDIR(char buf[])
+INODE *getInode(int ino)
+{
+	int blk = 0;
+	int offset;
+	char buf[BLK];
+	blk = ((ino - 1) / 8) + gp->bg_inode_table;
+	offset = (ino - 1) % 8;
+	getblk((u16)blk, buf);
+	putc('3');
+	return (INODE *)buf + offset;
+}
+
+int getIno(INODE *tip, char buf[], char * target)
 {
 	int i = 0; 
 	char *cp; 
 	int tblk = 0;
-	prints("In printDIR\n");
 	for ( i = 0; i < 12; i++)
 	{
-		tblk = ip->i_block[i];
+		tblk = tip->i_block[i];
 		if(tblk <= 0)
 		{
-			prints("block number invalid\n");
-			return (u16)1;
+			prints("did not found target: "); prints(target); prints(", returning\n");
+			return -1;
 		}
-
-		prints("getting new block: \n");
 		getblk((u16)tblk, buf);
 		cp = buf;
 		dp = (DIR *)buf;
 		while(cp < buf + 1024)
 		{
-			printsn(dp->name, dp->name_len);
-			prints(" ");
+			if(!strncmp(dp->name, target, dp->name_len))
+			{
+				
+				prints("Found: "); prints(target); prints(" folder\n");
+				return dp->inode;
+			}
 			cp = cp + dp->rec_len;
 			dp = (DIR *)cp;
 			
@@ -68,37 +68,25 @@ u16 printDIR(char buf[])
 	
 	
 }
-  /******** write C code to do these: ********************
-  (1).read first inode block into buf1[ ]
-
-  (2).let ip point to root INODE (inode #2)
-   
-  (3).For each DIRECT block of / do:
-          read data block into buf2[ ];
-          step through the data block to print the names of the dir entries 
-  
-  (4).prints("\n\rAll done\n\r");
-  ******************************************************/
 
 main()
 { 
-  u16  i, iblk;
-  char buf1[BLK], buf2[BLK];
+	u16  i, iblk; int ino = 0; INODE *tip;
+	char buf1[BLK], buf2[BLK];
+	
+	prints("booter start\n");  
+	/* read blk#2 to get group descriptor 0 */
+	getblk((u16)2, buf1);
+	gp = (GD *)buf1;
+	iblk = (u16)gp->bg_inode_table; // typecast u32 to u16
 
-  prints("booter start\n");  
 
-  /* read blk#2 to get group descriptor 0 */
-  getblk((u16)2, buf1);
-  gp = (GD *)buf1;
-  iblk = (u16)gp->bg_inode_table; // typecast u32 to u16
-
-  prints("inode_block= "); putc(iblk+'0'); //getc();
-  prints("\n");
-
-  prints("getting root inode.\n");
-  getblk((u16)iblk, buf1);
-  ip = (INODE *)buf1 + 1; 
-  printDIR(buf2);
+	getblk((u16)iblk, buf1);
+	ip = (INODE *)buf1 + 1; 
+	ino = getIno(ip, buf2, "boot");
+	tip = getInode(ino);
+  
+  
   
   
 }  
